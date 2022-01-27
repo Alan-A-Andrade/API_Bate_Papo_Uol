22064be5fb86a87164b2c19575cd52836ba7db15
lostzilla <alanaa92@gmail.com>
2022-01-27 13:54:36 -0300

    add mongoDB as storage data instead of computer memory

--- back-end/index.js
+++ back-end/index.js
@@ -2,20 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import Joi from 'joi';
 import dayjs from 'dayjs';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 const app = express();
-
 app.use(express.json())
 app.use(cors());
 
 const participantsOnline = []
 const chatMessages = []
 
+//mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb
+
 
 app.get("/hello", (req, res) => {
   res.send("Oi");
 });
-
 
 /* Participants Routs */
 
@@ -37,35 +42,61 @@ app.post("/participants", async (req, res) => {
 
     newParticipant.lastStatus = Date.now()
 
-    participantsOnline.push(newParticipant)
+    try {
+      await mongoClient.connect()
+      const dbUol = mongoClient.db("bate_papo_uol_alan");
+      const participantsCollection = dbUol.collection("participants")
+      const messagesCollection = dbUol.collection("messages")
 
-    chatMessages.push({
-      from: newParticipant.name,
-      to: 'Todos',
-      text: 'entra na sala...',
-      type: 'status',
-      time: dayjs(newParticipant.lastStatus).format('HH:mm:ss')
-    })
+      await participantsCollection.insertOne(newParticipant)
 
-    res.sendStatus(201)
+      let newStatusMsg = {
+        from: newParticipant.name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: dayjs(newParticipant.lastStatus).format('HH:mm:ss')
+      }
+
+      await messagesCollection.insertOne(newStatusMsg)
+
+      res.sendStatus(201)
+
+    } catch {
+      res.sendStatus(500)
+    }
+
 
   } catch {
     res.sendStatus(422)
   }
 
-});
-
-app.get("/participants", (req, res) => {
-
-  let participantsList = participantsOnline.map(el => {
-    let container = {}
-    container.name = el.name
-    return container
-  })
-
-  res.send(participantsList)
+  mongoClient.close()
 
 });
 
+app.get("/participants", async (req, res) => {
+
+  try {
+
+    await mongoClient.connect()
+    const dbUol = mongoClient.db("bate_papo_uol_alan");
+    const participantsCollection = dbUol.collection("participants")
+
+    const participantsOnline = await participantsCollection.find({}).toArray()
+
+    let participantsList = participantsOnline.map(el => {
+      let container = {}
+      container.name = el.name
+      return container
+    })
+    res.send(participantsList)
+  } catch {
+    res.sendStatus(500)
+  }
+
+});
+
+/* Messages Routs */
 
 app.listen(5000);
